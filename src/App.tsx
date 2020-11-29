30ac48792994f5a15cc96d1cbea6534bb59d1324
Hurricane0 <nikitadv777@gmail.com>
2020-11-29 18:04:13 +0200

    Created global ui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import {
+  Button,
+  createMuiTheme,
+  makeStyles,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@material-ui/core';
+import { green, orange } from '@material-ui/core/colors';
+
+const useStyles = makeStyles({
+  heading: {
+    textAlign: 'center',
+    marginTop: '40px',
+  },
+});
+
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[400],
+    },
+    secondary: {
+      main: green[400],
+    },
+  },
+});
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <Typography></Typography>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Typography className={classes.heading} variant="h1" component="h1">
+          Clicked button
+        </Typography>
+        <Button color="primary">orange</Button>
+        <Button color="primary">green</Button>
+      </div>
+    </ThemeProvider>
   );
 };
 
