6c7ba0425fa0cef8188a78181c5e1442cf9f34b2
Hurricane0 <nikitadv777@gmail.com>
2020-12-08 14:18:22 +0200

    Added ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/reduxStore';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {},
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
